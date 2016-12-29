b87a63c68f19065319545b4dae9e115b64a81c9e
Marshall Davis <toothlessrebel@gmail.com>
2016-12-29 01:24:13 -0500

    Removes redundant middleware and fixes response.
     Some middleware was duplicated on specific routes after the addition
    of the group. Also, this ensures API responses are the correct JSON
    format.

--- routes/api.php
+++ routes/api.php
@@ -23,9 +23,9 @@ Route::group(['middleware' => 'auth:token'], function () {
     Route::patch('/user/{user}', 'User@update');
 
     // Gender Routes
-    Route::patch('/gender/{gender}/replace', 'GenderSwap')->middleware('auth');
-    Route::delete('/gender/{gender}', 'Gender@delete')->middleware('auth');
-    Route::post('/genders', 'Gender@create')->middleware('auth');
+    Route::patch('/gender/{gender}/replace', 'GenderSwap');
+    Route::delete('/gender/{gender}', 'Gender@delete');
+    Route::post('/genders', 'Gender@create');
 
     // Subscription Routes
     Route::post('/newsletters/subscription', 'Newsletters@create');
