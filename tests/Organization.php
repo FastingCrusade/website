e2c4b778eb2ae818f6035b7996b46cb9163dbfb2
Marshall Davis <toothlessrebel@gmail.com>
2016-12-31 23:21:23 -0500

    Adds tests for Organizations model.

---
+++ tests/Organization.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ToothlessRebel
+ * Date: 31-Dec-16
+ * Time: 04:15
+ */
+
+namespace Testing;
+
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Models\Organization as OrganizationModel;
+use Illuminate\Support\Collection;
+
+class Organization extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testUser()
+    {
+        /** @var User $user */
+        $user = factory('App\Models\User')->create();
+        /** @var OrganizationModel $organization */
+        $organization = factory('App\Models\Organization')->create([
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertTrue($organization->user() instanceof BelongsTo);
+        $this->assertEquals($user->id, $organization->user->id);
+    }
+
+    public function testUsers()
+    {
+        /** @var OrganizationModel $organization */
+        $organization = factory('App\Models\Organization')->create([]);
+        /** @var Collection $users */
+        $users = factory('App\Models\User', 3)->create();
+        $users->each(function (User $user) use ($organization) {
+            $user->organizations()->attach($organization->id);
+            $user->save();
+        });
+
+        $this->assertTrue($organization->users() instanceof BelongsToMany);
+        $this->assertEquals($users->count(), $organization->users()->count());
+    }
+}
