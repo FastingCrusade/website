c3416726b54ff7de21f734291d6e58dfbdd76c97
Marshall Davis <toothlessrebel@gmail.com>
2017-01-02 01:22:57 -0500

    Adds create Fast route

--- app/Http/Controllers/Api/Fasts.php
+++ app/Http/Controllers/Api/Fasts.php
@@ -10,6 +10,8 @@ namespace App\Http\Controllers\Api;
 
 
 use App\Models\Fast;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class Fasts extends ApiController
 {
@@ -18,5 +20,19 @@ class Fasts extends ApiController
         $pagination = Fast::paginate(config('app.results_per_page'));
 
         return $this->response($pagination);
+    }
+
+    public function create(Request $request)
+    {
+        /** @var Fast $fast */
+        $fast = Fast::create($request->all());
+
+        if ($fast) {
+            $response = $this->response($fast->id, 'OK', Response::HTTP_CREATED);
+        } else {
+            $response = $this->response('Failed to create a Fast.', 'FAILED', Response::HTTP_NOT_ACCEPTABLE);
+        }
+
+        return $response;
     }
 }
