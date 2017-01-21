7ab758aa87ef98b1ba1501f843b3d692c42ca20d
Marshall Davis <toothlessrebel@gmail.com>
2017-01-21 00:36:24 -0500

    Expands CommentsController tests.

--- tests/CommentsController.php
+++ tests/CommentsController.php
@@ -12,7 +12,6 @@ namespace Testing;
 use App\Models\Comment;
 use App\Models\User as UserModel;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Http\Response;
 
 class CommentsController extends TestCase
 {
@@ -20,10 +19,12 @@ class CommentsController extends TestCase
 
     public function testDelete()
     {
-        /** @var Comment $comment */
-        $comment = factory('App\Models\Comment')->create();
         /** @var UserModel $user */
         $user = factory('App\Models\User')->create();
+        /** @var Comment $comment */
+        $comment = factory('App\Models\Comment')->create([
+            'user_id' => $user->id,
+        ]);
 
         $this->delete("/api/comments/{$comment->id}", [], ['Authorization' => "Bearer {$user->api_token}"]);
         $this->assertResponseOk();
@@ -34,10 +35,12 @@ class CommentsController extends TestCase
 
     public function testUpdate()
     {
-        /** @var Comment $comment */
-        $comment = factory('App\Models\Comment')->create();
         /** @var UserModel $user */
         $user = factory('App\Models\User')->create();
+        /** @var Comment $comment */
+        $comment = factory('App\Models\Comment')->create([
+            'user_id' => $user->id,
+        ]);
 
         $this->post(
             "/api/comments/{$comment->id}",
@@ -52,5 +55,23 @@ class CommentsController extends TestCase
 
         $comment = $comment->fresh();
         $this->assertEquals('This is an edited comment.', $comment->contents);
+    }
+
+    public function testDeleteAsAdmin()
+    {
+        /** @var UserModel $admin */
+        $admin = factory('App\Models\User')->states(['admin'])->create();
+        /** @var UserModel $user */
+        $user = factory('App\Models\User')->create();
+        /** @var Comment $comment */
+        $comment = factory('App\Models\Comment')->create([
+            'user_id' => $user->id,
+        ]);
+
+        $this->delete("/api/comments/{$comment->id}", [], ['Authorization' => "Bearer {$admin->api_token}"]);
+        $this->assertResponseOk();
+
+        $comment = $comment->fresh();
+        $this->assertTrue($comment->trashed());
     }
 }
